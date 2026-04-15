8016d6056ca1363a6bee411247394fb9ff0c391a
Andreas Kissner <andikiss500@gmail.com>
2026-04-15 20:39:29 +0200

    feat: add routes for home and survey-detail

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Home } from './features/home/home';
+import { SurveyDetail } from './features/survey-detail/survey-detail';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path : '', component:Home},
+    {path : '/survey/:id' , component : SurveyDetail }
+];
