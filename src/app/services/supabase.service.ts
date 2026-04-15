64d98da226c1e7afbe85cac53fadc926ea6b38c9
Andreas Kissner <andikiss500@gmail.com>
2026-04-15 21:01:02 +0200

    feat: connect Supabase client via environment config

--- src/app/services/supabase.service.ts
+++ src/app/services/supabase.service.ts
@@ -1,6 +1,11 @@
 import { Injectable } from '@angular/core';
+import { createClient } from '@supabase/supabase-js'
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
-export class SupabaseServiceTs {}
+export class Supabase {
+
+supabase = createClient(environment.supabaseUrl, environment.supabaseKey)
+}
