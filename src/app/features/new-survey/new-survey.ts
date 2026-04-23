69918864511f587ddd28bd254ef5f0cd3491732e
Andreas Kissner <andikiss500@gmail.com>
2026-04-23 19:12:58 +0200

    feat: new-survey page base HTML structure with header, fields and publish button

--- src/app/features/new-survey/new-survey.ts
+++ src/app/features/new-survey/new-survey.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { Header } from '../../shared/header/header';
+import { RouterLink } from '@angular/router';
+import { UiButtonComponent } from '../../shared/ui-button/ui-button';
 
 @Component({
   selector: 'app-new-survey',
-  imports: [Header],
+  imports: [Header,RouterLink, UiButtonComponent],
   templateUrl: './new-survey.html',
   styleUrl: './new-survey.scss',
 })
