dfad2102e440bae0c75870441203826b50467f59
Andreas Kissner <andikiss500@gmail.com>
2026-04-25 10:28:34 +0200

    Feat: Add in new-survey border-radius to all input field an begin with resopnsiv 1024px

--- src/app/features/home/survey-card-large/survey-card-large.ts
+++ src/app/features/home/survey-card-large/survey-card-large.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-survey-card-large',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './survey-card-large.html',
   styleUrl: './survey-card-large.scss',
 })
